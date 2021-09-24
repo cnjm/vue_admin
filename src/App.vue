e6f12dd494a59e864fed6818987863d44fdcb63f
cnjm <1837847333@qq.com>
2021-09-24 10:13:55 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+  // let a = 1
   import HelloWorld from './components/HelloWorld.vue'
 </script>
 
